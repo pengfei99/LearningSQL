64f167746753daab8fcf43a71aa70f86b7b928d3
pengfei <liu.pengfei@hotmail.fr>
2022-02-23 15:27:24 +0000

    prepare for datalab

---
+++ Insee/course/fundamentals/retriving_data/hints.py
@@ -0,0 +1,6 @@
+# -------------------------------------------------------------------------- #
+#
+# The standard format for a select statement that returns all columns and all rows is
+# Select * from TableName
+#
+# -------------------------------------------------------------------------- #
